c30a2bd9dbd30ba003b6207471426a58e8ea9a02
Diego <diego.melo4@fatec.sp.gov.br>
2025-09-11 09:58:28 -0300

    new(teste):tudo foi de vasco

--- TesteJava.java
+++ TesteJava.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class TesteJava {
     public static void main (String[] args) {
-        System.out.println("teste");
+        int a,b, resultado;
+        Scanner leitor = new Scanner(System.in);
+        System.out.printf("Digite um número\n");
+        a = leitor.nextInt();
+        
+        System.out.printf("Digite outro número:\n");
+        b = leitor.nextInt();
+        
+        resultado = a + b;
+        
+        System.out.printf("%d + %d  = %d",a,b,resultado);
+        
+        
+        leitor.close();
     }
 }
