c0e313a84623e5de43cc96f25dafe19c63b4b30b
Diego <diego.melo4@fatec.sp.gov.br>
2025-09-18 10:39:44 -0300

    teste(2)

---
+++ Personagem.java
@@ -0,0 +1,78 @@
+public class Personagem {
+  String nome;
+  private int energia = 10;
+  private int fome = 0;
+  private int sono = 0;
+
+  // Construtor com nome, energia, fome, sono
+  Personagem(String nome, int energia, int fome, int sono) {
+    this(energia,fome,sono);
+    this.nome = nome;
+  }
+
+  // Construtor só com nome
+  Personagem(String nome) {
+    this();
+    this.nome = nome;
+  }
+
+  // Construtor padrão
+  Personagem () {
+    energia = 10;   // faltava ponto e vírgula
+    fome = 0;
+    sono = 0;
+  }
+
+  // Construtor com energia, fome e sono
+  Personagem(int energia, int fome, int sono) {
+    if (energia >=0 && energia <=10) {
+      this.energia = energia;
+    }
+    if (fome >=0 && fome <=10) {
+      this.fome = fome;
+    }
+    if(!(sono < 0 || sono > 10)) {
+      this.sono = sono; 
+    }
+  }
+
+  void cacar() {
+    if(energia >= 2) {
+      System.out.println(nome + " caçando...");
+      energia -= 2;
+    }
+    else {
+      System.out.println(nome + " sem energia para caçar...");
+    }
+    fome = Math.min(fome + 1,10);
+    sono = Math.min(sono + 1,10);
+  }
+
+  void comer() {
+    if(fome>=1) {
+      System.out.printf("%s comendo...\n",nome);
+      --fome;
+      energia = energia < 10 ? energia + 1 : energia;
+    }
+    else {
+      System.out.printf("%s sem fome..\n",nome);
+    }
+  }
+
+  void dormir() {
+    if (sono >= 1) {
+      System.out.println(nome + " dormindo...");
+      sono -= 1;
+      if (energia < 10) {
+        energia++;
+      }
+    } else {
+      System.out.print(nome + " sem sono....\n");
+    }
+  }
+
+  @Override
+  public String toString() {
+    return String.format("%s -> e: %d, f: %d, s: %d",nome,energia,fome,sono);
+  }
+}
